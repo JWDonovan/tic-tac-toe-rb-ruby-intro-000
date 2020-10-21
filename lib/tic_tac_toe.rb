c895432c4d475b907f7a5e7e00903052d2c877bf
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-21 02:36:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ def turn(board)
   index = input_to_index(input)
 
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player)
     display_board(board)
   else
     turn(board)
